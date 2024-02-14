e5c3999e1f9368c2b79d8779b9076ff89bbdcb65
Felipe <fvhmanhaes@gmail.com>
2024-02-14 20:36:15 -0300

    ex02 feito

---
+++ M1S04/ex02.js
@@ -0,0 +1,10 @@
+let var1 = 25;
+
+console.log(var1);
+
+let answer = window.prompt('Escreva o novo valor de var1. \nAtual: '+ var1);
+var1 = answer;
+
+textShown = 'O novo valor de var1 é: '+ var1;
+console.log(textShown);
+window.alert(textShown);
