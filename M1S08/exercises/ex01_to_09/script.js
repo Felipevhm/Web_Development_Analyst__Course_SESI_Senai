0c52797536fe9363490276eb847266883ead2223
Felipe <fvhmanhaes@gmail.com>
2024-03-10 19:28:42 -0300

    M1S08 ex02 done.

--- M1S08/exercises/ex01_to_09/script.js
+++ M1S08/exercises/ex01_to_09/script.js
@@ -1,3 +1,13 @@
+// ex 01
+
 let changeBckg = document.getElementById('foo')
 
 changeBckg.style.background = '#467997'
+
+//ex 02
+
+let changeLiClass = document.getElementsByClassName('li-group')
+
+Array.from(changeLiClass).forEach(element => {
+  element.textContent = "This text was changed on ex02"
+});
