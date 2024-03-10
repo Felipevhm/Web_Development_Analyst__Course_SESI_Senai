09b85bc4a3072b2bf689f6d8771e564590ecf27e
Felipe <fvhmanhaes@gmail.com>
2024-03-10 19:38:37 -0300

    M1S08 ex03 done.

--- M1S08/exercises/ex01_to_09/script.js
+++ M1S08/exercises/ex01_to_09/script.js
@@ -11,3 +11,16 @@ let changeLiClass = document.getElementsByClassName('li-group')
 Array.from(changeLiClass).forEach(element => {
   element.textContent = "This text was changed on ex02"
 });
+
+//ex03
+
+let mainElement = document.querySelector("main")
+
+ex03Button = document.createElement('button')
+ex03Button.textContent = "Button Created on ex03"
+ex03Button.style.cssText ="width: 200px; height: 50px"
+
+mainElement.appendChild(ex03Button)
+
+
+
