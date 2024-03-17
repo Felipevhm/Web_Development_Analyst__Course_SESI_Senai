a969641222a4cea3968e9e25ff08a884eb8f8666
Felipe <fvhmanhaes@gmail.com>
2024-03-17 17:19:39 -0300

    M1S05 ex02 done.

--- M1S05/script.js
+++ M1S05/script.js
@@ -1,7 +1,28 @@
+let products = 
+[
+{code: 1, name:'strawberry', price: 1 },
+{code: 2, name:'oat', price: 2 },
+{code: 3, name:'juice', price: 1.5 }
+]
+
+let outputLabel = document.querySelector('label');
+
 function checkPrice() {
-  // Add your code here to check the price
-  alert('Price checked!');
+let input = document.getElementById('input-product')
+
+let foundElement = products.find(product => product.code === Number(input.value) || product.name === input.value);
+// console.log ("foundElement é: " + foundElement)
+if(foundElement){
+    
+    outputLabel.textContent =`Code #${foundElement.code} | ${foundElement.name}: $` + foundElement.price
+    console.log (outputLabel.textContent)  
+  }
+else{
+  outputLabel.textContent ="NOT FOUND"
+  console.log ("Product not found.")
 }
+}
+
 function buyProduct() {
   // Add your code here to buy the product
   alert('Product purchased!');
