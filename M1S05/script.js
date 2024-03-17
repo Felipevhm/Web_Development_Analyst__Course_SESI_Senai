b3f1d724d85ab82b7533860a2b475c3b64b71dc1
Felipe <fvhmanhaes@gmail.com>
2024-03-17 15:43:56 -0300

    M1S05 ex01 done.

---
+++ M1S05/script.js
@@ -0,0 +1,8 @@
+function checkPrice() {
+  // Add your code here to check the price
+  alert('Price checked!');
+}
+function buyProduct() {
+  // Add your code here to buy the product
+  alert('Product purchased!');
+}
