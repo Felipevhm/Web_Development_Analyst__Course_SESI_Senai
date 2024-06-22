80df30839a454c7e19a96767b3ed2b58a8896eb5
Felipe <fvhmanhaes@gmail.com>
2024-06-22 20:35:07 -0300

    Finish ex02 m02s04

---
+++ M02S04/ex02_m02s04.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes (nome, cidade, idade) VALUES
+('Lucas Lima','Salvador', 29)
+
+UPDATE clientes SET cidade = 'Fortaleza' WHERE cliente_id = 3;
+
+DELETE FROM pedidos WHERE valor < 150;
