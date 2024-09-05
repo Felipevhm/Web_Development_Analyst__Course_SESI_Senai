ace4ad8c2f7434f54b8da4e0468d070180c5a1a1
Felipe <fvhmanhaes@gmail.com>
2024-09-05 11:21:07 -0300

    Finish m03s01 ex05

--- M03S01/src/pages/Home.jsx
+++ M03S01/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import CountdownTimer from "../components/CountdownTimer"; // Importa o componente de temporizador
 
 const Home = () => {
   const { user, logout } = useContext(AuthContext);
@@ -11,6 +12,8 @@ const Home = () => {
         <div>
           <p>Bem-vindo, {user.name}!</p>
           <button onClick={logout}>Sair</button>
+          {/* Adiciona o componente de temporizador com um valor inicial de 5 minutos */}
+          <CountdownTimer initialMinutes={1} />
         </div>
       ) : (
         <p>Você não está logado.</p>
