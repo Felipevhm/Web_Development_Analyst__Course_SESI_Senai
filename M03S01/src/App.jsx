f50884b6da671e30f6d726bac5ba995ee4ffa9a6
Felipe <fvhmanhaes@gmail.com>
2024-09-05 10:34:33 -0300

    Organize folders and add finisherd ex01 for m03s01

---
+++ M03S01/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="container">
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
